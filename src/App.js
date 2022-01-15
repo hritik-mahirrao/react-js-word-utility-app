ce076480655f888f88681c2e40bb1772a2653498
Hritik Mahirrao <hritiknarendra@gmail.com>
2022-01-15 11:44:59 +0530

    theme changing funcutionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar title="Text Utils" />
       <div className="container">
         <TextForm heading="Enter text to analyze" />
+        <About/>
       </div>
     </>
   );
